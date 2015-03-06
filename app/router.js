a0b3102031cdc00a6db3cf8f09ca9fadcd49062b
Matt Bertino <mattbertino@gmail.com>
2015-03-06 16:46:06 -0500

    route file setup

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('mailer', function() {
+    this.route('front', { path: '/' });
+    this.route('back');
+    this.route('area');
+    this.route('deliver');
+  });
 });
 
 export default Router;
