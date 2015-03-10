9330f7f8aed95e80cf958137d1302b0afbdc5b82
Matt Bertino <mattbertino@gmail.com>
2015-03-10 13:23:35 -0400

    wip: on dropzone upload styling

--- app/components/c-dropzone.js
+++ app/components/c-dropzone.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  willInsertElement: function(){
+  didInsertElement: function(){
+
     // this gets rid of the auto discovery problem
     Dropzone.autoDiscover = false,
+
     // Instantiation of the dropzone container
-    this.$('#dropzone-container').dropzone({ url: "/file/post" });
+    this.$('#dropzone-container').dropzone({
+      thumbnailWidth: 945,
+      thumbnailHeight: 525,
+      url: "/file/post",
+      // clickable: false
+
+    });
+
   }
 });
